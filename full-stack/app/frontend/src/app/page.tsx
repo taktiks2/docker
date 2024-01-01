47fc5e3d44637df36a38b22ebbf13f2c0e7c9de2
taktiks2 <deathproof.lee@gmail.com>
2024-01-01 20:16:31 +0900

    add backend

--- full-stack/app/frontend/src/app/page.tsx
+++ full-stack/app/frontend/src/app/page.tsx
@@ -1,10 +1,24 @@
 "use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import axios from "axios";
 
 export default function Home() {
-  console.log("test");
+  const [test, setTest] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get(
+        "https://dummy.restapiexample.com/api/v1/employees",
+      );
+      setTest(res);
+    })();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button onClick={() => console.log(test)}>test</Button>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
